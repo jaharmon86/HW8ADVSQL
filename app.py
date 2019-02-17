9438f817f667f6fe9c9328071434b5e3662b16ee
jaharmon86 <james.h4rmon@gmail.com>
2019-02-16 23:07:15 -0700

    HW 8 FINAL

--- app.py
+++ app.py
@@ -11,38 +11,52 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 
-def get_session_and_tables():
-    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-    Base = automap_base()
-    Base.prepare(engine, reflect=True)
-    Measurement = Base.classes.measurement
-    Station = Base.classes.station
-    session = Session(engine)
-    return (session, Measurement, Station)
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False})
 
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session from Python to the DB
+session = Session(engine)
+
+#################################################
+# Set up Flask and landing page
+#################################################
 app = Flask(__name__)
+
+# last 12 months variable
+last_twelve_months = '2016-08-23'
 
 @app.route("/")
 def homepage():
     # thread gets created to service the request
-    """List of all returnable API routes."""
+    """List all avaiable API routes."""
     return(
-        f"(Note: Dates range from 2010-01-01 to 2017-08-23). <br><br>"
+        f"(Dates ranges from 2010-01-01 to 2017-08-23). <br><br>"
         f"Available Routes: <br>"
 
         f"/api/v1.0/precipitation<br/>"
-        f"Returns dates and temperature from the last year. <br><br>"
+        f"Returns dates and temperature from last year. <br><br>"
 
         f"/api/v1.0/stations<br/>"
-        f"Returns a json list of stations. <br><br>"
+        f"Returns json list of stations. <br><br>"
 
         f"/api/v1.0/tobs<br/>"
         f"Returns list of Temperature Observations(tobs) for previous year. <br><br>"
 
-        f"/api/v1.0/yyyy-mm-dd/<br/>"
+        f"/api/v1.0/<start><br/>"
         f"Returns an Average, Max, and Min temperatures for a given start date.<br><br>"
 
-        f"/api/v1.0/yyyy-mm-dd/yyyy-mm-dd/<br/>"
+        f"/api/v1.0/<start>/<end><br/>"
         f"Returns an Average, Max, and Min temperatures for a given date range."
 
         
@@ -54,29 +68,19 @@ def homepage():
 # So we don't throw some programming error on Windows machines
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    # connection to the db, session, tables
-    session, Measurement, Station = get_session_and_tables()
-    """Return Dates and Temp from the last year."""
-    precip_analysis = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= "2016-08-23").\
-        filter(Measurement.date <= "2017-08-23").all()
+    # Date 12 months ago
+    p_results = session.query(Measurement.date, func.avg(Measurement.prcp)).filter(Measurement.date >= last_twelve_months).group_by(Measurement.date).all()
+    return jsonify(p_results)
 
-    # creates JSONified list
-    precipitation_list = [precip_analysis]
 
-    return jsonify(precipitation_list)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # connection to the db, session, tables
-    session, Measurement, Station = get_session_and_tables()
-    """Return a list of stations"""
-    active_station = session.query(Measurement.station, Station.name, func.count(Measurement.tobs)).\
-    filter(Measurement.station == Station.station).group_by(Measurement.station).order_by(func.count(Measurement.tobs).desc()).all()
-    # results = session.query(Station.name, Station.station, Station.elevation).all()
+    s_results = session.query(Station.station, Station.name).all()
      
     # creates JSONified list of dictionaries
     station_list = []
-    for result in active_station:
+    for result in s_results:
         row = {}
         row['name'] = result[0]
         row['station'] = result[1]
@@ -86,16 +90,12 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def temp_obs():
-    # connection to the db, session, tables
-    session, Measurement, Station = get_session_and_tables()
-    """Return a list of tobs for the previous year"""
-    results = session.query(Station.name, Measurement.date, Measurement.tobs).\
-        filter(Measurement.date >= "2016-08-24", Measurement.date <= "2017-08-23").\
-        all()
+    t_results = session.query(Measurement.date, Measurement.station, Measurement.tobs).filter(Measurement.date >= last_twelve_months).all()
+    return jsonify(t_results)
 #
     # creates JSONified list of dictionaries
     tobs_list = []
-    for result in results:
+    for result in t_results:
         row = {}
         row["Station"] = result[0]
         row["Date"] = result[1]
@@ -107,14 +107,12 @@ def temp_obs():
 
 @app.route('/api/v1.0/<date>/')
 def given_date(date):
-    session, Measurement, Station = get_session_and_tables()
-    """Return the average temp, max temp, and min temp for the date"""
-    results = session.query(func.avg(Measurement.tobs), func.max(Measurement.tobs), func.min(Measurement.tobs)).\
-        filter(Measurement.date >= date).all()
+    day_temp_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= date).all()
+    return jsonify(day_temp_results)
 
     # creates JSONified list of dictionaries
     data_list = []
-    for result in results:
+    for result in day_temp_results:
         row = {}
         row['Start Date'] = date
         row['End Date'] = '2017-08-23'
@@ -127,14 +125,12 @@ def given_date(date):
 
 @app.route('/api/v1.0/<start_date>/<end_date>/')
 def query_dates(start_date, end_date):
-    session, Measurement, Station = get_session_and_tables()
-    """Return the avg, max, min, temp over a specific time period"""
-    results = session.query(func.avg(Measurement.tobs), func.max(Measurement.tobs), func.min(Measurement.tobs)).\
-        filter(Measurement.date >= start_date, Measurement.date <= end_date).all()
+    multi_day_temp_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    return jsonify(multi_day_temp_results)
 
     # creates JSONified list of dictionaries
     data_list = []
-    for result in results:
+    for result in multi_day_temp_results:
         row = {}
         row["Start Date"] = start_date
         row["End Date"] = end_date
